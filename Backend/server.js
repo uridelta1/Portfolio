fb58fbd35ee253e416ffb7d09abe4d28bbad80cd
uridelta1 <uridelta1@gmail.com>
2026-08-11 12:04:29 +0530

    initial commit 03

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
+const cors = require("cors")
 require('dotenv').config()
 const app=require('./src/app')
 const ConnectDB=require('./src/config/db')
 
-
+app.use(cors({
+    origin:process.env.CLIENT_ORIGIN,
+    credentials:true
+}))
 const PORT=process.env.PORT || 3000
 
 ConnectDB()
